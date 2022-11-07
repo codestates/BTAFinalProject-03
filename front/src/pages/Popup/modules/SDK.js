273bcf4dacddccbac0d096ec432399af5e3bbff4
DevJihwan <jh552016@naver.com>
2022-11-07 20:47:42 +0900

    feat : sdk 추가(거래내역, 테스트 토큰 조회)

--- front/src/pages/Popup/modules/SDK.js
+++ front/src/pages/Popup/modules/SDK.js
@@ -1,8 +1,8 @@
 import * as bip39 from '@scure/bip39';
 import { wordlist } from "@scure/bip39/wordlists/english";
-import { JsonRpcProvider, Ed25519Keypair, Ed25519PublicKey, RawSigner, PublicKey, Coin } from '@mysten/sui.js';
+import { JsonRpcProvider, Ed25519Keypair, Ed25519PublicKey, RawSigner, PublicKey, Coin, Network } from '@mysten/sui.js';
 
-const provider = new JsonRpcProvider('https://fullnode.devnet.sui.io');
+const provider = new JsonRpcProvider(Network.DEVNET);
 
 export class SDK {
     static generateMnemonic() {
@@ -11,6 +11,9 @@ export class SDK {
         return mnemonics;
     }
 
+    /*
+    * 니모닉을 통한 공개키 발급
+    */
     static async getPair(mnemonics) {
         const seed64Bytes = await bip39.mnemonicToSeed(mnemonics);
         const seed32Bytes = seed64Bytes.slice(0, 32);
@@ -20,6 +23,9 @@ export class SDK {
         return keypair;
     }
 
+    /*
+    * 공개키 노출 
+    */
     static getAddress(publicKey) {
         const address = new Ed25519PublicKey(publicKey).toSuiAddress();
         console.log(address);
@@ -28,6 +34,9 @@ export class SDK {
         return "0x" + address;
     }
 
+    /*
+    * 송금
+    */
     static async sendToken(keypair) {
         const signer = new RawSigner(keypair, provider);
 
@@ -45,6 +54,9 @@ export class SDK {
         return result;
     }
 
+    /*
+    * 잔액조회 
+    */
     static async getBalance() {
         const objects = await provider.getObjectsOwnedByAddress('0x6f03a907c902323c09040eaadb2d71d24dc8f1fb');
 
@@ -64,6 +76,9 @@ export class SDK {
         return result;
     }
 
+    /*
+    * 보유하고 있는 수이 리스트 
+    */
     static async getOwnSuiList() {
 
         //테스트용 공개키 주소, 파라미터로 공개키 받을 것 
@@ -109,6 +124,128 @@ export class SDK {
         }
 
 
+    /*
+    * 거래 히스토리를 보여주는 방법 
+    * 01. 보유하고 있는 수이 코인 객체를 불러옴. 
+    * 02. 각 객체의 digest를 가져오고, 
+    * 03. transaction data 에서 필요한 정보를 가져옴. 
+    */
+    static async getHistoryTranssaction(){
+
+
+        //함수 테스트용, 실제 연결할때는 파라미터를 공개키로 받고 아래 코드 대체 예정 
+        console.log("Staring get transaction history");
+        const pubkey = '0x52a2abe8940ae83a48e707a4d583db5b8e40a2b5';
+
+        //공개키로 모든 digest 내역 가져오기
+        const resultOfdiget = await provider.getTransactionsForAddress(pubkey);
+        // console.log("resultOfdiget : "+resultOfdiget);
+        console.log("resultOfdiget : "+resultOfdiget.length);
+
+        // 이 데이터로 원하는 데이터 뽑아냄 {return : flag: sent or receiver, recipients : 받은 사람, amount : 받은 돋, gas: 수수료}
+        // flag는 recipents의 주소가 입력한 공개키와 일치하면 받은 돈, 불일치하면 보낸 돈으로 판단 
+
+        let _flag, _recipients, _amount, _gas;
+        const map = new Map();
+
+        for (let i=0; i<resultOfdiget.length; i++){
+            //digest로 transaction 디테일 불러오기
+            const history = await provider.getTransactionWithEffects(resultOfdiget[i]);
+            console.log(`${i}번째 digest : ${resultOfdiget[i]}`);
+    
+            //불러온 내역 중에서 transactions만 빼오기
+            const selector = history.certificate.data.transactions;
+            // console.log("selector : " + JSON.stringify(selector));
+    
+    
+            //PaySui가 여러개 토큰을 발행해주는 메소드로 판단되어 분기 처리 
+            const methodName = JSON.stringify(selector).split("\"")[1];
+            console.log("methodName : "+ methodName);
+    
+            //json data 대괄호를 없애기 위한 작업
+            const a = JSON.stringify(selector[0]);
+            // console.log("a : " + a);
+    
+            _gas = history.effects.events[0].coinBalanceChange.amount;
+            // console.log("_gas : "+_gas);
+    
+            const data = JSON.parse(a);
+
+            if(methodName == "PaySui"){
+                // console.log("data.Paysui.amounts : "+ data.PaySui.amounts);
+    
+                //amount가 리스트로 내려오면 내려온 값 합산
+                if(1<data.PaySui.amounts.length){
+                    
+                    const subtotal = data.PaySui.amounts.reduce((accumulator, current) => {
+                        return accumulator + current;
+                    }, 0)
+                    
+                    // console.log("subtotal : "+subtotal);
+                    _amount = subtotal;
+                }else{
+                    _amount = data.PaySui.amounts;
+                }
+    
+                // console.log("data.Paysui.recipients : "+data.PaySui.recipients);
+    
+                //recipients가 복수로 내려오는 경우 확인 (테스트 토큰 지급시 수령인 5개가 모두 동일 주소로 내려옴)
+                if(1<data.PaySui.recipients.length){
+                    _recipients = data.PaySui.recipients[0];
+                }else{
+                    _recipients = data.PaySui.recipients;
+                }
+    
+                //수령인과 공개키를 비교하여 플래그 결정 
+                if(pubkey == _recipients){
+                    _flag = "Received"
+                }else{
+                    _flag = "Sent"
+                }
+    
+            }else if (methodName == "Pay"){
+                // console.log("data.Pay.amounts : "+data.Pay.amounts);
+                _amount = data.Pay.amounts;
+    
+                // console.log("data.Pay.recipients : "+data.Pay.recipients);
+                _recipients = data.Pay.recipients;
+    
+                //수령인과 공개키를 비교하여 플래그 결정 
+                if(pubkey == _recipients){
+                    _flag = "Received"
+                }else{
+                    _flag = "Sent"
+                }
+            }
+            
+            console.log(`_flag : ${_flag}, _recipients : ${_recipients}, _amount : ${_amount}, _gas : ${_gas}`);
+            map.set(i, [_flag, _recipients, _amount, _gas]);
+
+        }
+        return map;
+
+    }
+
+    /*
+    * 테스트 토큰 요청하기
+    */
+    static async getRequestTestToken(publicKey){
+
+        console.log("STRAT Get sui ");
+        const _pubkey = publicKey;
+
+        const _provider = new JsonRpcProvider(Network.DEVNET, {
+            // you can also skip providing this field if you don't plan to interact with the faucet
+            faucetURL: 'https://faucet.devnet.sui.io',
+          });
+
+        _provider.requestSuiFromFaucet(_pubkey)
+        .then((result) => {
+            console.log("response : " + result);
+        });
+
+
+    }
 
 
     }
